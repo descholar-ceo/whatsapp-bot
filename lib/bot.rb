79bc8684d90a84e6599587eb08e0961e5e631208
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-11 10:30:30 +0200

    Fix other err

--- lib/bot.rb
+++ lib/bot.rb
@@ -7,7 +7,7 @@ class WhatsAppBot < Sinatra::Base
     response = Twilio::TwiML::MessagingResponse.new
     response.message do |msg|
       if msg.start_with? 'translate'
-        msg.body("That's great, I will send you a solution soon")
+        msg.body('That\'s great, I will send you a solution soon')
       else
         msg.body('Cool! But that is not clear to me! Sorry!')
       end
