48437c6616e46d05aa38e02c907df985fd27e3b1
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-11 04:30:31 +0200

    bot_spec

--- spec/bot_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'telegram/bot'
-require_relative '../lib/bot'
-require_relative '../lib/modules/constants'
-
-describe Bot do
-  it 'when it is called, it works' do
-    expect(described_class).to be
-  end
-end
